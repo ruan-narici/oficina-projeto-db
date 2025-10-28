662a75e21206bb68f719907d1de0636ef8994e07
ruan-narici <ruannarici@hotmail.com>
2025-10-27 22:44:22 -0300

    feat: ADD V0013__insert_tbl_veiculos

---
+++ scripts/V0013__insert_tbl_veiculos.sql
@@ -0,0 +1,11 @@
+INSERT INTO tbl_veiculos (ds_modelo, nr_ano, id_cliente) VALUES
+('Toyota Corolla', 2020, 1),
+('Honda Civic', 2019, 2),
+('Chevrolet Onix', 2022, 3),
+('Volkswagen Gol', 2018, 4),
+('Fiat Toro', 2023, 5),
+('Jeep Compass', 2021, 6),
+('Hyundai HB20', 2020, 7),
+('Renault Kwid', 2022, 8),
+('Ford Ranger', 2021, 9),
+('Chevrolet S10', 2019, 10);
