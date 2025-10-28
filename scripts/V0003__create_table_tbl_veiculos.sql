618c44496e3d15e80bccfa6eeb699118eeb4e108
ruan-narici <ruannarici@hotmail.com>
2025-10-27 22:01:52 -0300

    fix: Change type for id_cliente V003

--- scripts/V0003__create_table_tbl_veiculos.sql
+++ scripts/V0003__create_table_tbl_veiculos.sql
@@ -2,7 +2,7 @@ CREATE TABLE tbl_veiculos (
   id_veiculo BIGSERIAL,
   ds_modelo VARCHAR(255) NOT NULL,
   nr_ano INTEGER NOT NULL,
-  id_cliente BIGSERIAL NOT NULL,
+  id_cliente BIGINT NOT NULL,
   PRIMARY KEY(id_veiculo),
   CONSTRAINT fk_tbl_veiculos_tbl_clientes_id_cliente FOREIGN KEY(id_cliente) REFERENCES tbl_clientes(id_cliente)
 )
