e1bc2cff9c026c53495abcf3b51b1cf25a87c540
ruan-narici <ruannarici@hotmail.com>
2025-10-27 21:55:08 -0300

    feat: ADD V0003__create_table_tbl_veiculos

---
+++ scripts/V0003__create_table_tbl_veiculos.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tbl_veiculos (
+  id_veiculo BIGSERIAL,
+  ds_modelo VARCHAR(255) NOT NULL,
+  nr_ano INTEGER NOT NULL,
+  id_cliente BIGSERIAL NOT NULL,
+  PRIMARY KEY(id_veiculo),
+  CONSTRAINT fk_tbl_veiculos_tbl_clientes_id_cliente FOREIGN KEY(id_cliente) REFERENCES tbl_clientes(id_cliente)
+)
