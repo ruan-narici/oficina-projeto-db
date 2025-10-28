203073a847bb0a01eb616f432bbf894b0f0dd395
ruan-narici <ruannarici@hotmail.com>
2025-10-27 22:43:45 -0300

    feat: ADD V0012__insert_tbl_clientes

---
+++ scripts/V0012__insert_tbl_clientes.sql
@@ -0,0 +1,11 @@
+INSERT INTO tbl_clientes (nm_cliente, nr_telefone) VALUES
+('Lucas Almeida', '(77) 99123-4578'),
+('Mariana Santos', '(11) 98765-4321'),
+('Carlos Henrique', '(21) 99888-7766'),
+('Fernanda Oliveira', '(31) 99234-5678'),
+('João Pedro Silva', '(85) 98811-2233'),
+('Aline Rocha', '(71) 99922-3344'),
+('Rafael Costa', '(41) 99333-2211'),
+('Beatriz Mendes', '(62) 99444-5566'),
+('Gustavo Ferreira', '(19) 99555-7788'),
+('Patrícia Nogueira', '(27) 99666-8899');
