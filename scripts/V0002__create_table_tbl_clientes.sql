a7e405a66f27debeaea8364e8fb096f7c3b4d2f0
ruan-narici <ruannarici@hotmail.com>
2025-10-27 21:54:02 -0300

    feat: ADD V0002__create_table_tbl_clientes

--- scripts/V0002__create_table_tbl_clientes.sql
+++ scripts/V0002__create_table_tbl_clientes.sql
@@ -1,5 +1,6 @@
 CREATE TABLE tbl_clientes (
   id_cliente BIGSERIAL,
   nm_cliente VARCHAR(150) NOT NULL,
-  nr_telefone VARCHAR(20) NOT NULL
+  nr_telefone VARCHAR(20) NOT NULL,
+  PRIMARY KEY(id_cliente)
 )
